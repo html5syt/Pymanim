d3ac2f1a6754b7beae31addec020679190b9980f
html5syt <html5syt@163.com>
2023-10-06 21:57:47 +0800

    tb

--- scene.py
+++ scene.py
@@ -1,8 +1,11 @@
 from manim import *
-
-
-class CreateCircle(Scene):
+from manim.utils.unit import Percent,Pixels
+class test(Scene):
     def construct(self):
-        circle = Circle()  # create a circle
-        circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
-        self.play(Create(circle))  # show the circle on screen
+        gr=NumberPlane(x_range=(-10,10),y_range=(-10,10))
+        self.play(FadeIn(gr))
+        for perc in range(5,51,5):
+            c=Circle(radius=perc*Percent(X_AXIS))
+            self.play(Create(c),run_time=0.3)
+            s=Square(side_length=2*perc*Percent(Y_AXIS),color=BLUE,fill_opacity=0.3)
+            self.play(Create(s),run_time=0.3)
